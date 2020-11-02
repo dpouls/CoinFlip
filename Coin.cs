750c011578d0ac994fdd2b1154a8c0bfbb51e391
Dallin <dallinpoulson@gmail.com>
2020-11-01 20:15:55 -0700

    finished project

---
+++ Coin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinFlip
+{
+    //create class called Coin
+    public class Coin
+    {
+        //instantiate Random so we can use it's methods
+        Random rand;
+        //delcare _sideup variable for our results
+        private string _sideup;
+        //create constructor method for Coin class
+        public Coin()
+        {
+            //instantiate random  when constructor is called
+            rand = new Random();
+            //set a default value to _sideup
+            _sideup = "Heads";
+        }
+        /// <summary>
+        /// Flips the coin using methods from the Random Class
+        /// </summary>
+        public void FlipCoin()
+        {
+            //get random whole number between 0 and 1
+            int randomSide = rand.Next(2);
+            //check what the number is
+            if (randomSide == 0)
+            {
+                //if 0 assign _sideup to Heads
+                _sideup = "Heads";
+            }
+            else
+            {
+                //if 1 assign _sideup to Tails
+
+                _sideup = "Tails";
+
+            }
+        }
+        /// <summary>
+        /// gives us the assigned value to _sideup
+        /// </summary>
+        /// <returns></returns>
+        public string GetSideUp()
+        {
+            //returns _sideup as a string
+            return _sideup;
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,9 +10,47 @@ namespace CoinFlip
 {
     public partial class MainPage : ContentPage
     {
+        //create new instance of coin class
+        Coin coin = new Coin();
+        //create new list of strings called coinResults
+        List<string> coinResults = new List<string>();
         public MainPage()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// use code from teh coin class to flip coin once
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnFlip_Clicked(object sender, EventArgs e)
+        {
+            //flipcoin in the coin class to flip the coin
+            coin.FlipCoin();
+            //alert that shows the result found in the getsideup method in coin class
+            DisplayAlert("Coin Results", coin.GetSideUp(), "Close");
+        }
+        /// <summary>
+        /// flip coin 5x with code from coin class.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnFlip5x_Clicked(object sender, EventArgs e)
+        {
+            //create new list of strings
+            LstViewCoin.ItemsSource = new List<string>();
+            //if we've already clicked this we want to clear the previous list
+            coinResults.Clear();
+            //loop through 5 times 
+            for (int i = 0; i < 5; i++)
+            {
+                //flip coin on each iteration with coin method FlipCoin
+                coin.FlipCoin();
+                //add result from getsideup to the coinResults list found on top of this page.
+                coinResults.Add(coin.GetSideUp());
+            }
+            //change the list to the results of looping through 5 times and getting 5 results added to the list.
+            LstViewCoin.ItemsSource = coinResults;
         }
     }
 }
